189f1613eae5ff8ad2046650657bbf5073d65ba4
percentcer <spence.miller@gmail.com>
2013-07-08 22:07:52 +0000

    set web.js to use index.html buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var f = fs.readFileSync('index.html'); 
+  response.send(f.toString());
 });
 
 var port = process.env.PORT || 5000;
